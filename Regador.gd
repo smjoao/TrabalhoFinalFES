2d7c08b6950594d0820ca3d26125facf3dcafa6a
unknown <joao_ps@dcc.ufrj.br>
2021-06-04 20:27:12 -0300

    arquivos deletados

--- Regador.gd
+++
@@ -1,8 +0,0 @@
-extends Node
-
-func do_action( fazenda ):
-	# verifica se a posição que o mouse aponta tem alguma planta
-	var planta = fazenda.check_for_planta( fazenda.mouse_tile_pos )
-	if planta:
-		# rega a planta naquela posição
-		planta.regar_planta()
